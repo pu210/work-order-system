0c0eeffcbfb1cdd69eb280a87af97c6b27632772
你的名字 <b27889933@gmail.com>
2026-08-08 17:40:22 +0800

    feat: 更新 POM 檔與設定並合併最新 dev 進度與葉子BEAN重複

--- work-order-backend/src/main/java/com/eeit219/work_order_system/modules/e/controller/ReportController.java
+++ work-order-backend/src/main/java/com/eeit219/work_order_system/modules/e/controller/ReportController.java
@@ -3,6 +3,7 @@ package com.eeit219.work_order_system.modules.e.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.eeit219.work_order_system.modules.e.service.ReportService;
 
 @RestController
 @RequestMapping("/api/reports")
+@CrossOrigin(origins = "*")
 public class ReportController {
 
     @Autowired
