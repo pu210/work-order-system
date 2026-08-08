bedb7a556eca7b3bce20b87bdc11f619c2898b67
h2322623226 <h2322623226@gmail.com>
2026-08-09 01:06:51 +0800

    feat: add WorkOrder get-by-id endpoint

--- work-order-backend/src/main/java/com/eeit219/work_order_system/modules/b/controller/WorkOrderController.java
+++ work-order-backend/src/main/java/com/eeit219/work_order_system/modules/b/controller/WorkOrderController.java
@@ -1,5 +1,7 @@
 package com.eeit219.work_order_system.modules.b.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,10 @@ public class WorkOrderController {
     public WorkOrderResponse create(@RequestBody WorkOrderCreateRequest request,
                                      @RequestParam Integer creatorUserId) {
         return workOrderService.create(request, creatorUserId);
+    }
+
+    @GetMapping("/{id}")
+    public WorkOrderResponse getById(@PathVariable Integer id) {
+        return workOrderService.getById(id);
     }
 }
