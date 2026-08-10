75f722f3b88c535a8e3d68cbdb65e4afbd00cb01
pu210 <a88073105672@gmail.com>
2026-08-10 12:12:32 +0800

    update role entity

--- work-order-backend/src/main/java/com/eeit219/work_order_system/modules/a/entity/Role.java
+++ work-order-backend/src/main/java/com/eeit219/work_order_system/modules/a/entity/Role.java
@@ -1,5 +1,8 @@
 package com.eeit219.work_order_system.modules.a.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.annotations.Nationalized;
 
 import jakarta.persistence.Column;
@@ -7,6 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import lombok.Getter;
@@ -19,6 +23,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name = "roles")
 public class Role {
+    @OneToMany(mappedBy = "role")
+    private List<UserRole> userRoles = new ArrayList<>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
