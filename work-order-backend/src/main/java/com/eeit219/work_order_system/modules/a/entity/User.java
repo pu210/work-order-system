d3022ee5bdfda520528586543e6d11469eb8836b
pu210 <a88073105672@gmail.com>
2026-08-10 12:11:04 +0800

    update user entity

--- work-order-backend/src/main/java/com/eeit219/work_order_system/modules/a/entity/User.java
+++ work-order-backend/src/main/java/com/eeit219/work_order_system/modules/a/entity/User.java
@@ -1,12 +1,18 @@
 package com.eeit219.work_order_system.modules.a.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hibernate.annotations.Nationalized;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -20,6 +26,26 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class User {
+
+    public final class UserStatus {
+
+        public static final byte DISABLED = 0; // 停用
+        public static final byte ACTIVE = 1; // 啟用
+        public static final byte PENDING = 2; // 待審核
+        public static final byte REJECTED = 3; // 審核未通過
+
+        private UserStatus() {
+        }
+    }
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserOauthAccount> oauthAccounts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PasswordResetToken> passwordResetTokens = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<UserRole> userRoles = new ArrayList<>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,6 +61,7 @@ public class User {
     @Column(name = "must_change_password", nullable = false)
     private Boolean mustChangePassword = true;
 
+    @Nationalized
     @Column(name = "name", nullable = false, length = 50)
     private String name;
 
@@ -45,7 +72,7 @@ public class User {
     private String phone;
 
     @Column(name = "status", nullable = false)
-    private Short status = 1;
+    private Byte status = 1;
 
     @Column(name = "created_time", nullable = false, updatable = false)
     private LocalDateTime createdTime;
