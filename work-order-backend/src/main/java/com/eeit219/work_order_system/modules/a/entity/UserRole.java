6bbf67995c12d60cefbf7c350d6a3b4c9010973f
pu210 <a88073105672@gmail.com>
2026-08-10 12:13:58 +0800

    update userrole entity

--- work-order-backend/src/main/java/com/eeit219/work_order_system/modules/a/entity/UserRole.java
+++ work-order-backend/src/main/java/com/eeit219/work_order_system/modules/a/entity/UserRole.java
@@ -5,6 +5,11 @@ import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
@@ -21,6 +26,18 @@ public class UserRole {
 
     @EmbeddedId
     private UserRoleId id;
+
+    @MapsId("userId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "FK_user_roles_users"))
+
+    private User user;
+
+    @MapsId("roleId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "role_id", nullable = false, foreignKey = @ForeignKey(name = "FK_user_roles_roles"))
+
+    private Role role;
 
     @Column(name = "created_time", nullable = false, updatable = false)
     private LocalDateTime createdTime;
