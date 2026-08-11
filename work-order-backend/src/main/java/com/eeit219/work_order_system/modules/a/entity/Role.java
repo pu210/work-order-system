72ca8c6f7b5aecb2c9eab8f73d96e674778f4c44
pu210 <a88073105672@gmail.com>
2026-08-11 16:16:12 +0800

    update role entity

--- work-order-backend/src/main/java/com/eeit219/work_order_system/modules/a/entity/Role.java
+++ work-order-backend/src/main/java/com/eeit219/work_order_system/modules/a/entity/Role.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.Nationalized;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,6 +24,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name = "roles")
 public class Role {
+    public static final String ADMIN = "ADMIN";
+    public static final String HANDLER = "HANDLER";
+    public static final String EMPLOYEE = "EMPLOYEE";
+
     @OneToMany(mappedBy = "role")
     private List<UserRole> userRoles = new ArrayList<>();
 
@@ -31,7 +36,7 @@ public class Role {
     @Column(name = "role_id", nullable = false)
     private Integer roleId;
 
-    @Column(name = "role_code", nullable = false, length = 20)
+    @Column(name = "role_code", nullable = false, length = 20, unique = true)
     private String roleCode;
 
     @Nationalized
