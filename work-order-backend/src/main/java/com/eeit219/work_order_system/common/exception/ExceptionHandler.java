ab99dca3e218e99096a0be7188d03541f2085c21
pu210 <a88073105672@gmail.com>
2026-08-11 16:25:05 +0800

    update ExceptionHandler.java

--- work-order-backend/src/main/java/com/eeit219/work_order_system/common/exception/ExceptionHandler.java
+++ work-order-backend/src/main/java/com/eeit219/work_order_system/common/exception/ExceptionHandler.java
@@ -1,5 +1,32 @@
 package com.eeit219.work_order_system.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.eeit219.work_order_system.common.response.ApiResponse;
+
+@RestControllerAdvice
 public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleResourceNotFound(
+            ResourceNotFoundException exception) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ApiResponse.error(
+                        HttpStatus.NOT_FOUND.value(),
+                        exception.getMessage()));
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<Void>> handleIllegalArgument(
+            IllegalArgumentException exception) {
+
+        return ResponseEntity.badRequest().body(
+                ApiResponse.error(
+                        HttpStatus.BAD_REQUEST.value(),
+                        exception.getMessage()));
+    }
 
 }
