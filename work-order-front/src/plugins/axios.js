fc644541f29f3761745850deb6d51ab41d0e05e4
pu210 <a88073105672@gmail.com>
2026-08-11 16:22:22 +0800

    update axios.js

--- work-order-front/src/plugins/axios.js
+++ work-order-front/src/plugins/axios.js
@@ -1,8 +1,32 @@
 import axios from "axios";
+import { clearAuth, getToken } from '@/utils/auth.js'
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080'
 });
+
+instance.interceptors.request.use((config) => {
+    const token = getToken()
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
+
+instance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        const status = error.response?.status
+        if (status === 401 && !error.config?.skipAuthRedirect) {
+            clearAuth()
+            const returnUrl = `${window.location.pathname}${window.location.search}`
+            window.location.assign(`/auth/login?returnUrl=${encodeURIComponent(returnUrl)}`)
+        } else if (status === 403) {
+            window.location.assign('/forbidden')
+        }
+        return Promise.reject(error)
+    }
+)
 
 
 export default instance;
