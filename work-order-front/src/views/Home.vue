c3f8bd81a8d2a3c4d2624be80a2cda6ec79012ad
pu210 <a88073105672@gmail.com>
2026-08-11 16:20:53 +0800

    update Home.vue

--- work-order-front/src/views/Home.vue
+++ work-order-front/src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
-    <h3>首頁</h3>
-    
+    <main class="container py-4">
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <h3>首頁</h3>
+                <p v-if="user" class="text-secondary mb-0">{{ user.account }}（{{ user.email }}）</p>
+            </div>
+            <button type="button" class="btn btn-outline-secondary" @click="logout">登出</button>
+        </div>
+    </main>
 </template>
     
 <script setup>
-	
+import { useRouter } from 'vue-router'
+import { clearAuth, getCurrentUser } from '@/utils/auth.js'
+
+const router = useRouter()
+const user = getCurrentUser()
+
+function logout() {
+    clearAuth()
+    router.replace({ name: 'login' })
+}
 </script>
     
 <style>
