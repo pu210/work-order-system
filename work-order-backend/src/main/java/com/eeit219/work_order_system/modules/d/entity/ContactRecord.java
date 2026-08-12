8742592bf36c9f103e6c44d973d2ed784da498f8
kkeei-9999 <sars200010235@gmail.com>
2026-08-12 23:42:58 +0800

    fix: correct entity relationship mappings

--- work-order-backend/src/main/java/com/eeit219/work_order_system/modules/d/entity/ContactRecord.java
+++ work-order-backend/src/main/java/com/eeit219/work_order_system/modules/d/entity/ContactRecord.java
@@ -1,14 +1,20 @@
 package com.eeit219.work_order_system.modules.d.entity;
 
+import com.eeit219.work_order_system.modules.a.entity.User;
+import com.eeit219.work_order_system.modules.b.entity.WorkOrder;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "contact_records")
-public class ContactRecords {
+public class ContactRecord {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,17 +22,24 @@ public class ContactRecords {
     private Integer recordId;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @Column(name = "author_user_id", nullable = false)
-    private Integer authorUserId;
+    @JoinColumn(name = "author_user_id", nullable = false)
+    private User author;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @Column(name = "work_order_id", nullable = false)
-    private Integer workOrder;
+    @JoinColumn(name = "work_order_id", nullable = false)
+    private WorkOrder workOrder;
 
     @Column(name = "content", nullable = false, length = 500)
     private String content;
 
     @Column(name = "created_time", nullable = false, updatable = false)
     private LocalDateTime createdTime;
+
+    @PrePersist
+    protected void onCreate() {
+        if (createdTime == null) {
+            createdTime = LocalDateTime.now();
+        }
+    }
 
 }
