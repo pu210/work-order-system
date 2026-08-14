b99bdfeb9bf2a1d49dd26d06b0a424b6c548ad07
Boooooomya <bon28083935@gmail.com>
2026-08-14 15:40:39 +0800

    Adjust API calls to extract data from nested response

--- work-order-front/src/api/category.js
+++ work-order-front/src/api/category.js
@@ -2,9 +2,9 @@ import api from '@/plugins/axios.js'
 
 // RepairCategoryController / SubCategoryController 直接回傳陣列，不包在 ApiResponse 裡
 export function getRepairCategories() {
-  return api.get('/api/repair-categories').then((res) => res.data)
+  return api.get('/api/repair-categories').then((res) => res.data.data)
 }
 
 export function getSubCategories() {
-  return api.get('/api/sub-categories').then((res) => res.data)
+  return api.get('/api/sub-categories').then((res) => res.data.data)
 }
