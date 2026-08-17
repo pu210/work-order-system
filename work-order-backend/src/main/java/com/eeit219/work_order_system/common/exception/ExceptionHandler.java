67dfcf9b2e2582fbcb138d66139b9b2a3095173b
pu210 <a88073105672@gmail.com>
2026-08-17 11:06:39 +0800

    refactor: remove useless import

--- work-order-backend/src/main/java/com/eeit219/work_order_system/common/exception/ExceptionHandler.java
+++ work-order-backend/src/main/java/com/eeit219/work_order_system/common/exception/ExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.eeit219.work_order_system.common.response.ApiResponse;
