607a9da67db116cb8bd1c6d0335bcd03dcfd07d9
kkeei-9999 <sars200010235@gmail.com>
2026-08-22 13:42:53 +0800

    feat：新增取得聯繫紀錄串接api

--- work-order-front/src/api/workOrderDetail.js
+++ work-order-front/src/api/workOrderDetail.js
@@ -1,7 +1,15 @@
-import api from '@/plugins/axios.js'
+import api from "@/plugins/axios.js";
 
+// 取得工單詳細資訊
 export function getWorkOrderDetail(workOrderId) {
-    return api
-        .get(`/api/work-orders/${workOrderId}/detail`)
-        .then((response) => response.data.data)
+  return api
+    .get(`/api/work-orders/${workOrderId}/detail`)
+    .then((response) => response.data.data);
+}
+
+// 取得聯繫紀錄
+export function getContactRecords(workOrderId) {
+  return api
+    .get(`/api/work-orders/${workOrderId}/contact-records`)
+    .then((response) => response.data.data);
 }
